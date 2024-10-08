355205c20fccda9e2b08a3badc555e36e4aa0c0b
ValentinaGutierrez16 <143976583+ValentinaGutierrez16@users.noreply.github.com>
2024-10-08 11:19:37 -0500

    politicas

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,27 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\Clientes;
+use App\Models\Compras;
+use App\Models\User;
+use App\Models\Role;
+use App\Models\Productos;
+use App\Models\Ventas;
+use App\Models\Proveedores;
+
+
+use App\Policies\ProveedoresPolicy;
+use App\Policies\ComprasPolicy;
+use App\Policies\VentasPolicy;
+use App\Policies\RolesPolicy;
+use App\Policies\UserPolicy;
+use App\Policies\ClientesPolicy;
+use App\Policies\ProductosPolicy;
+
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,6 +34,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         //
+        Clientes::class => ClientesPolicy::class,
+        Compras::class => ComprasPolicy::class,
+        Productos::class => ProductosPolicy::class,
+        Proveedores::class => ProveedoresPolicy::class,
+        Role::class => RolesPolicy::class,
+        User::class => UserPolicy::class,
+        Ventas::class => VentasPolicy::class,
     ];
 
     /**
